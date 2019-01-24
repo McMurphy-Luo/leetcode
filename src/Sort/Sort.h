ac8cd1a07ff23792b94f3c870c592dc063499188
McMurphy Luo <McMurphy.Luo@zoom.us>
2019-01-24 18:05:27 +0800

    Implement quick sort. Adding test cases.

--- src/Sort/Sort.h
+++ src/Sort/Sort.h
@@ -1,15 +1,56 @@
 #include <functional>
+#include <cassert>
+
+
+template<typename I, typename V>
+bool ArrayEquals(I begin_1, I end_1, I begin_2, I end_2, std::function<int(const v&, const V&)> comparator) {
+  assert(begin_1 < end_1);
+  assert(begin_2 < end_2);
+  assert(end_1 - begin_1 == end_2 - begin_2);
+
+  I current_1 = begin_1;
+  I current_2 = begin_2;
+
+  while (current_1 < end_1) {
+    if (comparator(*current_1, *current_2) != 0) {
+      return false;
+    }
+    ++current_1;
+    ++current_2;
+  }
+  return true;
+}
 
 template<typename I, typename V>
 void QuickSort(I begin, I end, std::function<int(const V&, const V&)> comparator) {
-  I anchor = begin;
-  while(anchor < end) {
-    if (*anchor < *begin) {
-      std::swap(*anchor, *begin);
-      ++anchor;
+  assert(begin <= end);
+  if (begin == end) {
+    return;
+  }
+  I anchor_left = begin;
+  I anchor_right = end - 1;
+  I pivot_position = begin;
+  V pivot = *begin;
+
+  while (anchor_left != anchor_right) {
+    while (anchor_left != anchor_right) {
+      if (*anchor_right < pivot) {
+        std::swap(*anchor_right, *pivot_position);
+        pivot_position = anchor_right;
+        break;
+      }
+      --anchor_right;
+    }
+    while (anchor_left != anchor_right){
+      if (*anchor_left > pivot) {
+        std::swap(*anchor_left, *pivot_position);
+        pivot_position = anchor_left;
+        break;
+      }
+      ++anchor_left;
     }
   }
 
-  
-
+  QuickSort<I, V>(begin, pivot_position, comparator);
+  QuickSort<I, V>(pivot_position + 1, end, comparator);
 }
--- src/Sort/SortTest.cpp
+++ src/Sort/SortTest.cpp
@@ -1,12 +1,31 @@
+#define CATCH_CONFIG_MAIN
 #include <iostream>
-#include "QuickSort.h"
+#include "catch.hpp"
+#include "Sort.h"
 
-int main(int argc, char* argv[]) {
-  std::cout << "hello world" << std::endl;
+const int* test_array_0 = nullptr;
+const int test_array_1[1] = {1};
+const int test_array_2[2] = {10, 3};
+const int test_array_3[2] = {3, 10};
+const int test_array_4[4] = {6, 984311654, 30, 0};
+const int test_array_5[13] = {31690, 0, 42346549, 0, 1659, 2, 689, 1, -123, 1, 1, 1239, 10};
 
+int IntegerComparator(const int& lhs, const int& rhs) {
+  if (lhs < rhs) {
+    return -1;
+  }
+  if (lhs > rhs) {
+    return 1;
+  }
+  return 0;
+}
+
+TEST_CASE("Test the quick sort algorithm") {
   int source[5] = {5, 12, 30, 9, 16};
-
-  
-
-
+  QuickSort<int*, int>(source, source + 5, IntegerComparator);
+  REQUIRE(source[0] == 5);
+  REQUIRE(source[1] == 9);
+  REQUIRE(source[2] == 12);
+  REQUIRE(source[3] == 16);
+  REQUIRE(source[4] == 30);
 }
