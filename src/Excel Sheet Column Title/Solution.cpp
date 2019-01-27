0b54b865370bd04ab7dd8984856c70b7100f3b4c
McMurphy Luo <McMurphy.Luo@gmail.com>
2019-01-27 11:55:29 +0800

    Correct the solution for problem 'Excel Sheet Column Title'

--- src/Excel Sheet Column Title/Solution.cpp
+++ src/Excel Sheet Column Title/Solution.cpp
@@ -1,26 +1,43 @@
+#define CATCH_CONFIG_RUNNER
+#include "../catch.hpp"
 #include <string>
+#include <algorithm>
 
 using std::string;
+using std::cin;
+using std::reverse;
 
 class Solution {
 public:
   string convertToTitle(int n) {
+    int reserved = n;
+    int quotient = 0;
     string result;
+    while (reserved > 0) {
+      quotient = reserved % 26;
+      reserved = reserved / 26;
 
-    int reminder = 0;
-    int quotient = n - 1;
-
-    if (quotient == 0) {
-      result.push_back('A');
+      if (quotient == 0 && reserved > 0) {
+        reserved = reserved - 1;
+        quotient = 26;
+      }
+      result.push_back(quotient - 1 + 'A');
     }
-
-    while (quotient > 0) {
-      reminder = n % 26;
-      quotient = quotient / 26;
-      char next_char = reminder + 'A';
-      result.push_back(next_char);
-    }
-    
+    reverse(result.begin(), result.end());
     return result;
   }
 };
+
+TEST_CASE("Test the House Robber problem") {
+  Solution test_instance;
+  REQUIRE(test_instance.convertToTitle(1) == "A");
+  REQUIRE(test_instance.convertToTitle(26) == "Z");
+  REQUIRE(test_instance.convertToTitle(27) == "AA");
+  REQUIRE(test_instance.convertToTitle(26 * 26) == "YZ");
+}
+
+int main(int argc, char* argv[]) {
+  int result = Catch::Session().run(argc, argv);
+  cin.get();
+  return result;
+}
