5d71b108c63c9f4e5f35d9c07f5669dad1820b17
McMurphy Luo <McMurphy.Luo@zoom.us>
2019-02-19 18:15:50 +0800

    Minor change to Project file of solution for problem 'Merge k Sorted Lists.'. Add a Project of solution for problem 'Count Complete Tree Nodes'

---
+++ src/Merge k Sorted Lists/Solution.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+#include <cstddef>
+#include <vector>
+
+using std::vector;
+
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+  ListNode *mergeKLists(vector<ListNode *> &lists)
+  {
+    return NULL;
+  }
+};
