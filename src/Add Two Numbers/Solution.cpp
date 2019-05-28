33371b5ed0635e2e89b2aa1236b1cfe8b6ac3903
McMurphy Luo <McMurphy.Luo@gmail.com>
2019-05-28 22:22:59 +0800

    Add project for problem 'Add Two Numbers'

---
+++ src/Add Two Numbers/Solution.cpp
@@ -0,0 +1,20 @@
+#define CATCH_CONFIG_MAIN
+#include "../catch.hpp"
+
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+ 
+class Solution {
+public:
+  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+  }
+};
+
+TEST_CASE() {
+
+}
