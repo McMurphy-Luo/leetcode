d72d5114593e24c22bf36c52918e5de2661a6da8
McMurphy Luo <McMurphy.Luo@gmail.com>
2019-06-03 22:37:45 +0800

    temporary commit, save my work

--- src/Longest Substring Without Repeating Characters/Solution.cpp
+++ src/Longest Substring Without Repeating Characters/Solution.cpp
@@ -2,13 +2,24 @@
 #include "../catch.hpp"
 
 #include <string>
+#include <map>
 
 using std::string;
+using std::map;
 
 class Solution {
 public:
   int lengthOfLongestSubstring(string s) {
-
+    map<char, size_t> unique_char_position;
+    int total_count = 0;
+    for (size_t i = 0; i < s.size(); ++i) {
+      char current = s.at(i);
+      map<char, size_t>::const_iterator char_in_map = unique_char_position.find(current);
+      if (char_in_map == unique_char_position.cend()) {
+        unique_char_position.emplace(std::make_pair(current, i));
+      }
+    }
+    return total_count;
   }
 };
 
