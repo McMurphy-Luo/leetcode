afb9a86a32dc71f13672262bfe7814590fbd0347
McMurphy Luo <McMurphy.Luo@gmail.com>
2019-06-05 22:48:23 +0800

    Finish the solution for problem 'Longest Substring Without Repeating Characters'

--- src/Longest Substring Without Repeating Characters/Solution.cpp
+++ src/Longest Substring Without Repeating Characters/Solution.cpp
@@ -3,23 +3,38 @@
 
 #include <string>
 #include <map>
+#include <algorithm>
 
 using std::string;
 using std::map;
+using std::max;
 
 class Solution {
 public:
   int lengthOfLongestSubstring(string s) {
-    map<char, size_t> unique_char_position;
-    int total_count = 0;
-    for (size_t i = 0; i < s.size(); ++i) {
-      char current = s.at(i);
-      map<char, size_t>::const_iterator char_in_map = unique_char_position.find(current);
-      if (char_in_map == unique_char_position.cend()) {
-        unique_char_position.emplace(std::make_pair(current, i));
-      }
+    size_t begin = 0;
+    size_t end = begin;
+    size_t total_count = 0;
+    if (s.size() == 1) {
+      return 1;
     }
-    return total_count;
+    while (end < s.size()) {
+      char current = s.at(end);
+      bool find_duplicated = false;
+      for (size_t i = begin; i < end; ++i) {
+        if (s.at(i) == current) {
+          total_count = max(total_count, end - begin);
+          begin = i + 1;
+          find_duplicated = true;
+          break;
+        }
+      }
+      if (!find_duplicated) {
+        total_count = max(total_count, end - begin + 1);
+      }
+      ++end;
+    }
+    return static_cast<int>(total_count);
   }
 };
 
@@ -28,4 +43,5 @@ TEST_CASE("Test the solution for problem 'Longest Substring Without Repeating Ch
   REQUIRE(solution_instance.lengthOfLongestSubstring("abcabcbb") == 3);
   REQUIRE(solution_instance.lengthOfLongestSubstring("bbbbb") == 1);
   REQUIRE(solution_instance.lengthOfLongestSubstring("pwwkew") == 3);
+  REQUIRE(solution_instance.lengthOfLongestSubstring("au") == 2);
 }
