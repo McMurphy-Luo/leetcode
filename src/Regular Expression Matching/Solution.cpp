12487373da594cff2dcac5536ad831e7aaa696ba
McMurphy Luo <McMurphy.Luo@gmail.com>
2019-06-30 22:27:53 +0800

    Add project for problem 'Regular Expression Matching'

---
+++ src/Regular Expression Matching/Solution.cpp
@@ -0,0 +1,18 @@
+/**
+ * @author McMurphy.Luo
+ * @date June/30/2019
+ * @brief The solution for problem https://leetcode.com/problems/regular-expression-matching/
+ */
+
+#define CATCH_CONFIG_MAIN
+#include "../catch.hpp"
+#include <string>
+
+using std::string;
+
+class Solution {
+public:
+  bool isMatch(string s, string p) {
+
+  }
+};
