03526d472183791a8d6116c0c2ce6a723e7723ee
McMurphy Luo <McMurphy.Luo@gmail.com>
2019-07-28 20:22:07 +0800

    Add project files for problem "Remove Nth Node From End of List" and problem "Letter Combinations of a Phone Number"

---
+++ src/Letter Combinations of a Phone Number/Solution.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN
+#include "../catch.hpp"
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class Solution {
+public:
+  vector<string> letterCombinations(string digits) {
+
+  }
+};
+
---
+++ src/Remove Nth Node From End of List/Solution.cpp
@@ -0,0 +1,17 @@
+#define CATCH_CONFIG_MAIN
+#include "../catch.hpp"
+#include <string>
+#include <vector>
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+  ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+  }
+};
