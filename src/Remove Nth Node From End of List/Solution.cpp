612c140f9b7b2563187c6226a508372cce480b4d
McMurphy Luo <McMurphy.Luo@gmail.com>
2019-07-28 21:12:34 +0800

    Complete the solution for problem "Remove Nth Node from End of List"

--- src/Remove Nth Node From End of List/Solution.cpp
+++ src/Remove Nth Node From End of List/Solution.cpp
@@ -12,6 +12,60 @@ struct ListNode {
 class Solution {
 public:
   ListNode* removeNthFromEnd(ListNode* head, int n) {
-
+    ListNode* the_node_to_be_removed = nullptr;
+    ListNode* the_node_before_to_be_removed = nullptr;
+    ListNode* current = head;
+    int distance = 0;
+    while (current) {
+      current = current->next;
+      ++distance;
+      if (distance == n) {
+        the_node_to_be_removed = head;
+      }
+      if (distance == n + 1) {
+        the_node_before_to_be_removed = head;
+      }
+      if (distance > n) {
+        the_node_to_be_removed = the_node_to_be_removed->next;
+      }
+      if (distance > n + 1) {
+        the_node_before_to_be_removed = the_node_before_to_be_removed->next;
+      }
+    }
+    if (!the_node_to_be_removed) {
+      return head;
+    }
+    if (the_node_to_be_removed && !the_node_before_to_be_removed) {
+      assert(the_node_to_be_removed == head);
+      return head->next;
+    }
+    assert(the_node_to_be_removed && the_node_before_to_be_removed);
+    // warning: memory leaks
+    the_node_before_to_be_removed->next = the_node_to_be_removed->next;
+    return head;
   }
 };
+
+void DeleteList(ListNode* head) {
+  if (head) {
+    DeleteList(head->next);
+    delete head;
+  }
+}
+
+TEST_CASE("Test the solution for problem \"Remove Nth Node From End of List\"") {
+  ListNode* list = new ListNode(1);
+  list->next = new ListNode(2);
+  list->next->next = new ListNode(3);
+  list->next->next->next = new ListNode(4);
+  list->next->next->next->next = new ListNode(5);
+
+  Solution sln_instance;
+  sln_instance.removeNthFromEnd(list, 2);
+  CHECK(list->val == 1);
+  CHECK(list->next->val == 2);
+  CHECK(list->next->next->val == 3);
+  CHECK(list->next->next->next->val == 5);
+  CHECK(list->next->next->next->next == NULL);
+  DeleteList(list);
+}
