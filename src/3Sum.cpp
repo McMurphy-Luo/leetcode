e0eeb41b14942732f9fb8e00afe1383de614e85a
McMurphy.Luo <McMurphy.Luo@zoom.us>
2019-08-06 13:08:03 +0800

    Temporary commit, save my work

--- src/3Sum.cpp
+++ src/3Sum.cpp
@@ -3,16 +3,27 @@
 #include <algorithm>
 
 using std::vector;
+using std::sort;
+using std::mismatch;
 
 namespace
 {
   bool UnorderedArrayEqual(const vector<int>& input_1, const vector<int>& input_2) {
-    return false;
+    if (input_1.size() != input_2.size()) {
+      return false;
+    }
+    vector<int> input_1_copy = input_1;
+    vector<int> input_2_copy = input_2;
+    sort(input_1_copy.begin(), input_1_copy.end());
+    sort(input_2_copy.begin(), input_2_copy.end());
+    return mismatch(input_1_copy.begin(), input_1_copy.end(), input_2_copy.begin()).first == input_1_copy.end();
   }
 
   bool VectorInVector(const vector<int>& input, const vector<vector<int>>& container) {
     for (vector<vector<int>>::const_iterator it = container.cbegin(); it != container.cend(); ++it) {
-
+      if (UnorderedArrayEqual(input, *it)) {
+        return true;
+      }
     }
     return false;
   }
@@ -26,5 +37,9 @@ public:
 };
 
 TEST_CASE("Test the solution for problem '3Sum'") {
-
+  Solution sln_instance;
+  vector<int> input {-1, 0, 1, 2, -1, -4};
+  vector<vector<int>> result = sln_instance.threeSum(input);
+  REQUIRE(VectorInVector(vector<int>{-1, 0, 1}, result));
+  REQUIRE(VectorInVector(vector<int>{-1, -1, 2}, result));
 }
