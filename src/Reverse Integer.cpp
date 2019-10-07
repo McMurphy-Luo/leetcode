fd425b91fb06d8a1570cb25954840b5c2808a398
McMurphy Luo <McMurphy.Luo@gmail.com>
2019-10-07 19:03:17 +0800

    Finish the solution for problem 'Reverse Integer'

--- src/Reverse Integer.cpp
+++ src/Reverse Integer.cpp
@@ -1,11 +1,19 @@
 #include "catch.hpp"
 #include <cstring>
+#include <cstdio>
+#include <sstream>
 
+using std::swap;
+using std::snprintf;
 
 namespace
 {
   void reverse_string(char* begin, char* end) {
-
+    while (begin < end) {
+      swap(*begin, *end);
+      ++begin;
+      --end;
+    }
   }
 }
 
@@ -13,17 +21,26 @@ class Solution {
 public:
   int reverse(int x) {
     char buf[64];
-    _itoa_s(x, buf, 10);
-    int char_count_to_ignore = 0;
-
-
-
-    while (*buf == '0') {
-      ++char_count_to_ignore;
+    memset(buf, 0, sizeof(buf));
+    int return_val = snprintf(buf, sizeof(buf), "%d", x);
+    if (return_val != -1) {
+      bool negtive = (buf[0] == '-');
+      reverse_string(buf + (negtive ? 1 : 0), buf + strnlen(buf, sizeof(buf)) - 1);
+      int char_count_to_ignore = negtive ? 1 : 0;
+      while (*(buf + char_count_to_ignore) == '0') {
+        ++char_count_to_ignore;
+      }
+      long long result = atoll(buf + char_count_to_ignore)* (negtive ? -1 : 1);
+      if (result >= INT_MAX || result <= INT_MIN) {
+        return 0;
+      }
+      else {
+        return result;
+      }
     }
-
-    
-    return 0;
+    else {
+      return 0;
+    }
   }
 };
 
@@ -32,4 +49,5 @@ TEST_CASE("Test the solution for problem \"Reverse Integer\"") {
   CHECK(solution_instance.reverse(123) == 321);
   CHECK(solution_instance.reverse(-123) == -321);
   CHECK(solution_instance.reverse(120) == 21);
+  CHECK(solution_instance.reverse(1534236469) == 0);
 }
