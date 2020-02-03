b8a36f8b87478f180f65955de90914b3821a85fb
McMurphy Luo <McMurphy.Luo@gmail.com>
2020-02-03 22:17:59 +0800

    Finish the solution for problem 'Combination Sum'

--- src/Combination Sum.cpp
+++ src/Combination Sum.cpp
@@ -1,15 +1,50 @@
 #include "catch.hpp"
 #include <vector>
-#include "Sort.h"
 
 using std::vector;
 
 class Solution {
 public:
   vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-    return {};
+    if (candidates.empty()) {
+      return {};
+    }
+    int current_run_value = candidates.back();
+    int count = 0;
+    vector<vector<int>> result;
+    vector<int> candidates_next = candidates;
+    candidates_next.pop_back();
+    while (count * current_run_value <= target) {
+      if (target - count * current_run_value == 0) {
+        result.push_back(vector<int>(count, current_run_value));
+        break;
+      }
+      vector<vector<int>> combination_of_subrouting = combinationSum(candidates_next, target - count * current_run_value);
+      for (
+        vector<vector<int>>::iterator it = combination_of_subrouting.begin();
+        it != combination_of_subrouting.end();
+        ++it
+      ) {
+        it->insert(it->end(), count, current_run_value);
+      }
+      result.insert(result.end(), combination_of_subrouting.cbegin(), combination_of_subrouting.cend());
+      ++count;
+    }
+    return result;
   }
 };
+
+bool ArraySame(const vector<int>& lhs, const vector<int>& rhs) {
+  if (rhs.size() != lhs.size()) {
+    return false;
+  }
+  for (vector<int>::const_iterator it = lhs.begin(); it != lhs.end(); ++it) {
+    if (find(rhs.begin(), rhs.end(), *it) == rhs.end()) {
+      return false;
+    }
+  }
+  return true;
+}
 
 bool Contains(const vector<vector<int>>& array_of_array, const vector<int>& target) {
   for (
@@ -17,7 +52,7 @@ bool Contains(const vector<vector<int>>& array_of_array, const vector<int>& targ
     it != array_of_array.cend();
     ++it
     ) {
-    if (ArrayEquals<vector<int>::const_iterator, int>(it->cbegin(), it->cend(), target.cbegin(), target.cend(), IntComparator)) {
+    if (ArraySame(*it, target)) {
       return true;
     }
   }
@@ -43,13 +78,15 @@ bool ArrayOfArraySame(const vector<vector<int>>& lhs, const vector<vector<int>>&
 TEST_CASE("Test the solution for problem \"Combination Sum\"") {
   Solution sln_instance;
   vector<int> input{ 2,3,6,7 };
-  vector<vector<int>> output{ {7}, {2, 2, 3} };
-  CHECK(ArrayOfArraySame(sln_instance.combinationSum(input, 7), output));
+  vector<vector<int>> expected_output{ {7}, {2, 2, 3} };
+  vector<vector<int>> output = sln_instance.combinationSum(input, 7);
+  CHECK(ArrayOfArraySame(expected_output, output));
   input = { 2,3,5 };
-  output = {
+  expected_output = {
     {2, 2, 2, 2},
     {2, 3, 3},
     {3, 5}
   };
-  CHECK(ArrayOfArraySame(sln_instance.combinationSum(input, 8), output));
+  output = sln_instance.combinationSum(input, 8);
+  CHECK(ArrayOfArraySame(expected_output, output));
 }
